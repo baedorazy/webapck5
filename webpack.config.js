739dabeaadb4e9162bc068ddc2b79ea0514535ee
jnbae <jnbae@jasongroup.co.kr>
2021-06-10 13:50:12 +0900

    개발버전 추가 config -> devtools inline-source-map 추가하면 디버깅 쉬움 어떤 js문제인지 몇라인 문제인지 알려줌

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
   	index: './src/index.js',
 	print: './src/print.js'
   },
+  devtool: 'inline-source-map',
   output: {
     filename: '[name].bundle.js',
 	path: path.resolve(__dirname, 'dist'),
@@ -13,7 +15,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-	  title: 'Output Management'
+	  title: 'Development',
 	}),
   ],
 };
