abdfe144ae03f7010b53b5a8980f2798ca8443f6
jnbae <jnbae@jasongroup.co.kr>
2021-06-15 15:05:27 +0900

    args setting

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 console.log('index.js')
 import _ from 'lodash';
-import printMe from './day2-spliting/print';
+import Print from './print';
+
 
 function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
 	 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  btn.innerHTML = 'Click me and check the console!';
-  element.onclick = printMe;
-	
-	element.appendChild(btn);
+  element.onclick = Print.bind(null, 'Hello webpack');
   
   return element;
 }
