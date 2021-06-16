925e2d8154539b22b40032151d7d3002b34a5915
jnbae <jnbae@jasongroup.co.kr>
2021-06-16 13:40:11 +0900

    multiple output setting

--- source/about.js
+++ source/about.js
@@ -1,6 +1,6 @@
 import hello_word from './hello.js';
 import world_word from './world.js';
 import css from './style.css';
-document.querySelector("#root").innerHTML = hello_word + world_word;
+document.querySelector("#root").innerHTML = world_word + ' ' + hello_word;
 
 console.log(css, ':css');
